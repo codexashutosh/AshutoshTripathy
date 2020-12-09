07fc9a867c8c7a8ee92f625fe37af9cb0cd45be5
Ashutosh Tripathy <standn8@gmail.com>
2020-12-10 04:24:40 +0530

    Added page

--- main.js
+++ main.js
@@ -2,4 +2,14 @@ $(".navbar li").on("click", function() {
 	$(".navbar li").removeClass("active");
 	$(this).addClass("active");
 });
-
+$('.section1nav').on('click', function() {
+	$(".sections").hide();
+    $(".section1").show();
+});
+$('.section2nav').on('click', function() {
+	$(".sections").hide();
+    $(".section2").show();
+});
+$('#section2nav').on('click', function() {
+	$('.section2nav').click();
+});
