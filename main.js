694bd9885354860497bbf98b7462568e55b8e207
Ashutosh Tripathy <standn8@gmail.com>
2020-12-10 23:23:50 +0530

    Configured layout and pages

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
+window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
 $(".navbar li").on("click", function() {
-	$(".navbar li").removeClass("active");
-	$(this).addClass("active");
+	$(".navbar button").addClass("collapsed");
+	$(".navbar button").attr("aria-expanded",false);
+	$(".navbar .navbar-collapse").removeClass("show");
 });
-$('.section1nav').on('click', function() {
-	$(".sections").hide();
-    $(".section1").show();
-});
-$('.section2nav').on('click', function() {
-	$(".sections").hide();
-    $(".section2").show();
-});
-$('#section2nav').on('click', function() {
-	$('.section2nav').click();
+$(function(){
+  $('.banner, .aboutme').css({ height: $(window).innerHeight() });
+  $(window).resize(function(){
+    $('.banner, .aboutme').css({ height: $(window).innerHeight() });
+  });
 });
