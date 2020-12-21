93488767c8df642b05f5afc8fe767eb6d09f397a
Ashutosh Tripathy <standn8@gmail.com>
2020-12-21 20:09:32 +0530

    Added customized cursor

--- main.js
+++ main.js
@@ -18,3 +18,11 @@ $(function(){
     $('.aboutme').css({ height: $(window).innerHeight() });
   });
 });
+let mouseCursor = document.querySelector(".cursor");
+
+window.addEventListener('mousemove',cursor);
+
+function cursor(e){
+    mouseCursor.style.top = e.pageY + "px";
+    mouseCursor.style.left = e.pageX + "px";
+}
