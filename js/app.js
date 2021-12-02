001e6ea1cca8e2a5002e80fef388998e0ff7227f
Ashutosh Tripathy <standn8@gmail.com>
2021-12-03 00:17:07 +0530

    Update navbar mobile

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ const navSlide =() =>{
                 link.style.animation = '';
             }
             else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`;
             }
         });
         // burger animation
