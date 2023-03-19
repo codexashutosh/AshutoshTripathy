d1c39d884b9f06822104a23c8939c788ecfb90b7
Ashutosh Tripathy <standn8@gmail.com>
2023-03-19 19:12:18 +0530

    Update styles

--- js/app.js
+++ js/app.js
@@ -1,10 +1,36 @@
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+const makeNavLinksSmooth = () => {
+    const navLinks = document.querySelectorAll('.navlink');
+    for (let n in navLinks) {
+        if (navLinks.hasOwnProperty(n)) {
+            navLinks[n].addEventListener('click', e => {
+                e.preventDefault();
+                document.querySelector(navLinks[n].hash)
+                .scrollIntoView({
+                    behavior: "smooth"
+                });
+                document.querySelector(".burger").click();
+            });
+        }
+    }
+}
 
-const navbar = document.querySelector('#navbar');
-const scrollspy = new VanillaScrollspy(navbar, 1000, 'easeInOutQuint');
-scrollspy.init();
+const spyScrolling = () => {
+    const sections = document.querySelectorAll('.page');
+    
+    window.onscroll = () => {
+        const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+        
+        for ( let s in sections )
+        if ( sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos ) {
+            const id = sections[s].id;
+            document.querySelector('.active').classList.remove('active');
+            document.querySelector(`a[href*=${ id }]` ).classList.add('active');
+        }
+    } 
+}
+
+makeNavLinksSmooth( );
+spyScrolling( );
 
 const navSlide =() =>{
     const burger = document.querySelector(".burger");
@@ -36,8 +62,8 @@ $("#loading").show().delay(1500).fadeOut(
         $('body').removeClass('stop-scrolling');
         $('body').unbind('touchmove');
     }
-);
-navSlide();
-// $('.see-desc').on('click',function(){
-//     $('.roles').slideToggle();  //swap the display of the main content with slide action
-//   });
+    );
+    navSlide();
+    // $('.see-desc').on('click',function(){
+    //     $('.roles').slideToggle();  //swap the display of the main content with slide action
+    //   });
